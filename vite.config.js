aa6c056a42818ae1ebeb34142880efa6d25ad112
gzaf5466 <gzaf5466@gmail.com>
2026-07-23 19:54:49 +0530

    Disable sourcemaps in vite.config.js to eliminate Tailwind CSS v4 build warning

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    sourcemap: true,
+    sourcemap: false,
   },
   server: {
     headers: {
